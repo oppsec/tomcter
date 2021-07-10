d09b2cb50afc7a722415ba31267c2a924cfaf3f4
oppsec <daniel.sdm.github@gmail.com>
2021-07-10 18:49:32 -0300

    fix: minor changes

--- src/design/ui.py
+++ src/design/ui.py
@@ -2,12 +2,20 @@ from rich import print
 from os import system, name
 
 
-def get_banner(file_name: str = "src/design/banner.txt") -> str:
-    with open(file_name) as banner_file:
-        file_content = banner_file.read()
+def banner() -> str:
+    """ Returns the application banner
+    """
 
-        print(f"[yellow][b]{file_content}[/][/]")
+    file: str = "src/design/banner.txt"
+
+    with open(file) as banner_file:
+        content = banner_file.read()
+
+        print(f"[yellow][b]{content}[/][/]")
 
 
 def clear() -> None:
-    system('cls' if name == 'nt ' else 'clear')
+    """ Clear the screen
+    """
+
+    system('cls' if name == 'nt' else 'clear')
