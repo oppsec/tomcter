7a46c281f18e73a31917c864b87f27c89f4c872d
oppsec <50470310+oppsec@users.noreply.github.com>
2021-07-10 17:04:55 -0300

    fix: minor changes

--- src/core/agent.py
+++ src/core/agent.py
@@ -1,10 +1,11 @@
 from random import randint
 
-def return_user_agent(file: str = "src/core/data/user-agents.txt") -> str:
+def return_user_agent() -> str:
     "Returns a random User-Agent"
 
-    with open(file) as agents_file:
+    file = "src/core/data/user-agents.txt"
 
+    with open(file) as agents_file:
         user_agents = agents_file.readlines()
         user_agents = user_agents[randint(0, len(user_agents) -1)]
         user_agents = user_agents.encode('utf-8')
