dae47772abf30183bd6a97437c43ab7ed644b9fa
oppsec <opps3c@gmail.com>
2021-07-25 13:47:17 -0300

    new: added argparser

--- main.py
+++ main.py
@@ -3,8 +3,15 @@
 from src.design.ui import banner, clear
 from src.core.main import connect
 
+from argparse import ArgumentParser
+
 
 if __name__ == "__main__":
     clear()
     banner()
-    connect()
+
+    parser = ArgumentParser()
+    parser.add_argument('-l', help="Text file with targets to bruteforce", required=True)
+    args = parser.parse_args()
+
+    connect(args)
